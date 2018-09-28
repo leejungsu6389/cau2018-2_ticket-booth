31870e3fd5af09899a68d30a20f79dc18b26d288
jungkimin <43000750+jungkimin@users.noreply.github.com>
2018-09-29 02:54:14 +0900

    라이브러리에 있는 배경 제거 함수 KNN, MOG2를 사용하여 영상속 물체의 움직임을 부각시켜보았습니다

--- main.cpp
+++ main.cpp
@@ -7,29 +7,44 @@
 #endif
 using namespace std;
 using namespace cv;
-int main() {
-	VideoCapture stream("walking.mp4");
+void processVideo(char* video_name) {
+	VideoCapture capture(video_name);
+	Ptr<BackgroundSubtractor> pKNN;
+	Ptr<BackgroundSubtractor> pMOG2;
+	Mat fgMaskMOG2;
+	Mat fgMaskKNN;
 	Mat frame;
-	Mat sub_frame;
-	Mat old_frame;
-	int flag = 1;
-	while(1) {
-		stream.read(frame);
-		if(frame.empty()) {
-			if(flag==1) {
-				flag = 0;
-				old_frame = frame.clone();
-			}
-			else {
-				break;
-			}
-		}
-		Sobel(frame,frame,CV_8U,1,0);
-		sub_frame = frame-old_frame;
-		imshow("walking",sub_frame);
-		waitKey(100);
-		old_frame = frame.clone();
+	double start;
+	double end_time;
+	pKNN = createBackgroundSubtractorKNN(500,400,true);
+	pMOG2 = createBackgroundSubtractorMOG2(500,400,true);
+	while(!capture.isOpened()) {
+		std::cout<<"error"<<std::endl;
+		exit(EXIT_FAILURE);
 	}
-	waitKey(0);
+	int count = 0;
+	int flag = 0;
+	start = clock();
+	
+	while(1) {
+		if(!capture.read(frame)) {
+			std::cout<<"file end"<<std::endl;
+			break;
+		}
+		pKNN->apply(frame,fgMaskKNN);
+		pMOG2->apply(frame,fgMaskMOG2);
+		imshow("KNN",fgMaskKNN);
+		imshow("MOG2",fgMaskMOG2);
+		if(count>100 && flag == 0) {
+			flag = 1;
+			double end = clock();
+		}
+		waitKey(100);
+		count++;
+	}
+}
+int main() {
+	char f_name[] = "People.mp4";
+	processVideo(f_name);
 	return 0;
 }
