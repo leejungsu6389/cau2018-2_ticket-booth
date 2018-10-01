1742c11a6aac2fcc22f39bc5588562a78d4e3c81
leejungsu <33692466+leejungsu6389@users.noreply.github.com>
2018-10-01 09:38:07 +0900

    (백업용) 서버 코드 작성중

---
+++ display.php
@@ -0,0 +1,24 @@
+﻿<?php
+
+    /* import PHP Excel library */
+
+
+    /* read current.xlsx */
+    /* read LOG.xlsx */
+
+
+
+    /* get HTTP request */
+    $s = $_POST["Signal"];
+
+
+    /* show current state */
+
+    /* show Logs */
+
+
+    /* LOG File download */
+
+
+
+?>
