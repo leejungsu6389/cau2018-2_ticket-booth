d96c84d82307bd7edb27aca5d81d042593a7ffac
jungkimin <43000750+jungkimin@users.noreply.github.com>
2018-10-15 15:10:17 +0900

    resize함수를 구현해본 코드입니다.
    RGB 3채널만 가능합니다.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <random>
-#include <math.h>
 #include <opencv2/opencv.hpp>
 #ifdef _DEBUG
 #pragma comment(lib, "opencv_world343d.lib")
@@ -9,102 +7,102 @@
 #endif
 using namespace std;
 using namespace cv;
+Mat k_downsize(int tar_r,int tar_c,Mat input) {
+	double Rr = input.rows/(double)tar_r;
+	double Rc = input.cols/(double)tar_c;
+	Mat output(tar_r,tar_c,CV_8UC3);
+	output.setTo(0);
+	for(int i = 0; i<tar_r; i++) {
+		int start_y = 0;
+		double start_range = 0.0;
+		int end_y = 0;
+		double end_range = 0.0;
+		int p = 0;
+		while(1) {
+			if(p+1>Rr*i) {
+				start_y = p;
+				start_range = p+1-(Rr*i);
+				break;
+			}
+			p++;
+		}
+		while(1) {
+			if(p+1>Rr *(i+1)) {
+				end_y = p;
+				end_range = Rr*(i+1)-p;
+				break;
+			}
+			p++;
+		}
+		for(int j = 0; j<tar_c; j++) {
+			int start_x = 0;
+			double xstart_range = 0.0;
+			int end_x = 0;
+			double xend_range = 0.0;
+			int q = 0;
+			while(1) {
+				if(q+1>Rc*j) {
+					start_x = q;
+					xstart_range = q+1-(Rc*j);
+					break;
+				}
+				q++;
+			}
+			while(1) {
+				if(q+1>Rc *(j+1)) {
+					end_x = q;
+					xend_range = Rc*(j+1)-q;
 
-Mat km_sobel(Mat input) {
-	Mat applied(input.rows-2,input.cols-2,input.type());
-	applied.setTo(0);
-	int sobelX[9] = {-1,0,1,-2,0,2,-1,0,1};
-	int sobelY[9] = {-1,-2,-1,0,0,0,1,2,1};
-	for(int i = 0; i<applied.rows; i++) {
-		for(int j = 0; j<applied.cols; j++) {
-			for(int c = 0; c<input.type(); c++) {
-				double x = 0;
-				double y = 0;
-				for(int p = 0; p<3; p++) {
-					for(int q = 0; q<3; q++) {
-						x += (double)input.data[(i+p)*input.step+(j+q)*input.elemSize()+c]*sobelX[p*3+q];
-						y += (double)input.data[(i+p)*input.step+(j+q)*input.elemSize()+c]*sobelY[p*3+q];
-					}
+					break;
 				}
-				if(x<0) {
-					x = -x;
-				}
-				if(y<0) {
-					y = -y;
-				}
-				applied.data[i*applied.step+j*applied.elemSize()+c] = 0.5*(x+y);
+				q++;
 			}
-		}
-	}
-	return applied;
-}
-Mat km_dilate(Mat input,int filt_size) {
-	Mat applied(input.rows-filt_size+1,input.cols-filt_size+1,input.type());
-	applied.setTo(0);
-	for(int i = 0; i<applied.rows; i++) {
-		for(int j = 0; j<applied.cols; j++) {
-			for(int c = 0; c<input.elemSize(); c++) {
-				int max = input.data[i*input.step+j*input.elemSize()+c];
-				for(int p = 0; p<filt_size; p++) {
-					for(int q = 0; q<filt_size; q++) {
-						if(max<input.data[(i+p)*input.step+(j+q)*input.elemSize()+c]) {
-							max = input.data[(i+p)*input.step+(j+q)*input.elemSize()+c];
+			double accumr = 0.0;
+			double accumg = 0.0;
+			double accumb = 0.0;
+			for(int n = start_y; n<=end_y; n++) {
+				for(int m = start_x; m<=end_x; m++) {
+					double ratio = 1.0;
+					if(start_y!=end_y) {
+						if(n==start_y) {
+							ratio *= start_range;
+						}
+						if(n==end_y) {
+							ratio *= end_range;
 						}
 					}
-				}
-				applied.data[i*applied.step+j*applied.elemSize()+c] = max;
-			}
-		}
-	}
-	return applied;
-}
-Mat cvDilation(Mat input,int erosion_size,int erosion_type) {
-	/*erosion_type = MORPH_RECT, MORPH_CROSS,MORPH_ELLIPSE*/
-	Mat output = input.clone();
-	Mat element = getStructuringElement(erosion_type,Size(2*erosion_size+1,2*erosion_size+1),Point(erosion_size,erosion_size));
-	dilate(input,output,element);
-	return output;
-}
-Mat cvErosion(Mat input,int erosion_size,int erosion_type) {
-	/*erosion_type = MORPH_RECT, MORPH_CROSS,MORPH_ELLIPSE*/
-	Mat output = input.clone();
-	Mat element = getStructuringElement(erosion_type,Size(2*erosion_size+1,2*erosion_size+1),Point(erosion_size,erosion_size));
-	erode(input,output,element);
-	return output;
-}
-Mat km_erode(Mat input,int filt_size) {
-	Mat applied(input.rows-filt_size+1,input.cols-filt_size+1,input.type());
-	applied.setTo(0);
-	for(int i = 0; i<applied.rows; i++) {
-		for(int j = 0; j<applied.cols; j++) {
-			for(int c = 0; c<input.elemSize(); c++) {
-				int min = input.data[i*input.step+j*input.elemSize()+c];
-				for(int p = 0; p<filt_size; p++) {
-					for(int q = 0; q<filt_size; q++) {
-						if(min>input.data[(i+p)*input.step+(j+q)*input.elemSize()+c]) {
-							min = input.data[(i+p)*input.step+(j+q)*input.elemSize()+c];
+					else {
+						ratio *= 1-(2-start_range-end_range);
+					}
+					if(start_x!=end_x) {
+						if(m==start_x) {
+							ratio *= xstart_range;
+						}
+						if(m==end_x) {
+							ratio *= xend_range;
 						}
 					}
+					else {
+						ratio *= 1-(2-xstart_range-xend_range);
+					}
+					accumr += (ratio)* input.data[(n*input.step)+(m*input.elemSize())+0];
+					accumg += (ratio)* input.data[(n*input.step)+(m*input.elemSize())+1];
+					accumb += (ratio)* input.data[(n*input.step)+(m*input.elemSize())+2];
 				}
-				applied.data[i*applied.step+j*applied.elemSize()+c] = min;
 			}
+			output.data[i*output.step+j*output.elemSize()+0] += (unsigned char)(accumr/(Rr*Rc));
+			output.data[i*output.step+j*output.elemSize()+1] += (unsigned char)(accumg/(Rr*Rc));
+			output.data[i*output.step+j*output.elemSize()+2] += (unsigned char)(accumb/(Rr*Rc));
 		}
 	}
-	return applied;
+	return output;
 }
-int main(void)
-{
-	Mat img = imread("avrill.jpg");
-	Mat kmDilate = km_dilate(img,9);
-	Mat kmErode = km_erode(img,9);
-	Mat kmsobel = km_sobel(img);
-	namedWindow("km_dilate",0);
-	namedWindow("km_erode",0);
-	namedWindow("kmsobel",0);
-	imshow("km_dilate",kmDilate);
-	imshow("km_erode",kmErode);
-	imshow("kmsobel",kmsobel);
+int main() {
+	Mat frame = imread("Avril.jpg",1);
+	Mat test = k_downsize(2000,2000,frame);
+	namedWindow("test",0);
+	imshow("avril",frame);
+	imshow("test",test);
 	waitKey(0);
-	return(0);
-
+	return 0;
 }
