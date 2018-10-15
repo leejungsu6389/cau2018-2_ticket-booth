d1002259952bf1c9f5e59f595fa6cd215c862480
jungkimin <43000750+jungkimin@users.noreply.github.com>
2018-10-15 15:03:13 +0900

    sobel,erode,dilate를 직접 구현해본 코드입니다.
    http://visionprogrammer.tistory.com/10를 참고하여 erode 와  dialte 함수를 만들어 봤습니다. 
    https://blog.naver.com/mym0404/221317601407를 참고하여 sobel edge detection 함수 을 만들어 봤습니다.,

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <random>
 #include <math.h>
 #include <opencv2/opencv.hpp>
-#include <opencv\highgui.h>
-#include <opencv\cvaux.h>
 #ifdef _DEBUG
 #pragma comment(lib, "opencv_world343d.lib")
 #else
@@ -11,6 +9,55 @@
 #endif
 using namespace std;
 using namespace cv;
+
+Mat km_sobel(Mat input) {
+	Mat applied(input.rows-2,input.cols-2,input.type());
+	applied.setTo(0);
+	int sobelX[9] = {-1,0,1,-2,0,2,-1,0,1};
+	int sobelY[9] = {-1,-2,-1,0,0,0,1,2,1};
+	for(int i = 0; i<applied.rows; i++) {
+		for(int j = 0; j<applied.cols; j++) {
+			for(int c = 0; c<input.type(); c++) {
+				double x = 0;
+				double y = 0;
+				for(int p = 0; p<3; p++) {
+					for(int q = 0; q<3; q++) {
+						x += (double)input.data[(i+p)*input.step+(j+q)*input.elemSize()+c]*sobelX[p*3+q];
+						y += (double)input.data[(i+p)*input.step+(j+q)*input.elemSize()+c]*sobelY[p*3+q];
+					}
+				}
+				if(x<0) {
+					x = -x;
+				}
+				if(y<0) {
+					y = -y;
+				}
+				applied.data[i*applied.step+j*applied.elemSize()+c] = 0.5*(x+y);
+			}
+		}
+	}
+	return applied;
+}
+Mat km_dilate(Mat input,int filt_size) {
+	Mat applied(input.rows-filt_size+1,input.cols-filt_size+1,input.type());
+	applied.setTo(0);
+	for(int i = 0; i<applied.rows; i++) {
+		for(int j = 0; j<applied.cols; j++) {
+			for(int c = 0; c<input.elemSize(); c++) {
+				int max = input.data[i*input.step+j*input.elemSize()+c];
+				for(int p = 0; p<filt_size; p++) {
+					for(int q = 0; q<filt_size; q++) {
+						if(max<input.data[(i+p)*input.step+(j+q)*input.elemSize()+c]) {
+							max = input.data[(i+p)*input.step+(j+q)*input.elemSize()+c];
+						}
+					}
+				}
+				applied.data[i*applied.step+j*applied.elemSize()+c] = max;
+			}
+		}
+	}
+	return applied;
+}
 Mat cvDilation(Mat input,int erosion_size,int erosion_type) {
 	/*erosion_type = MORPH_RECT, MORPH_CROSS,MORPH_ELLIPSE*/
 	Mat output = input.clone();
@@ -25,39 +72,39 @@ Mat cvErosion(Mat input,int erosion_size,int erosion_type) {
 	erode(input,output,element);
 	return output;
 }
-int main(void) {
-	VideoCapture stream("People.mp4");
-	Mat capture;
-	Ptr<BackgroundSubtractor> pMOG2;
-	vector<vector <Point>> contours;
-	vector<Vec4i> hierarchy;
-	char cau[] = "Ticket_Booth_cau";
-	while(1) {
-		if(!stream.read(capture)) {
-			break;
+Mat km_erode(Mat input,int filt_size) {
+	Mat applied(input.rows-filt_size+1,input.cols-filt_size+1,input.type());
+	applied.setTo(0);
+	for(int i = 0; i<applied.rows; i++) {
+		for(int j = 0; j<applied.cols; j++) {
+			for(int c = 0; c<input.elemSize(); c++) {
+				int min = input.data[i*input.step+j*input.elemSize()+c];
+				for(int p = 0; p<filt_size; p++) {
+					for(int q = 0; q<filt_size; q++) {
+						if(min>input.data[(i+p)*input.step+(j+q)*input.elemSize()+c]) {
+							min = input.data[(i+p)*input.step+(j+q)*input.elemSize()+c];
+						}
+					}
+				}
+				applied.data[i*applied.step+j*applied.elemSize()+c] = min;
+			}
 		}
-		Mat store = capture.clone();
-		cvtColor(capture,capture,CV_RGB2GRAY);
-		GaussianBlur(capture,capture,Size(7,7),1.5,1.5);
-		threshold(capture,capture,128,255,CV_THRESH_BINARY);
-		cvErosion(capture,10,MORPH_RECT);
-		cvDilation(capture,10,MORPH_RECT);
-		cvDilation(capture,10,MORPH_RECT);
-		cvErosion(capture,25,MORPH_RECT);
-		findContours(capture.clone(),contours,hierarchy,RETR_CCOMP,CHAIN_APPROX_SIMPLE);
-		vector<vector<Point> > contours_poly(contours.size());
-		vector<Rect> boundRect(contours.size());
-		for(int i = 0; i<contours.size(); i++) {
-			approxPolyDP(Mat(contours[i]),contours_poly[i],3,true);
-			boundRect[i] = boundingRect(Mat(contours_poly[i]));
-		}
-		for(int i = 0; i<contours.size(); i++) {
-			rectangle(store,boundRect[i].tl(),boundRect[i].br(),Scalar(0,255,0),1,8,0);
-		}
-		putText(store,cau,Point(30,20),CV_FONT_NORMAL,1,Scalar(0,0,0),2,1);
-		imshow("capture",store);
-		waitKey(100);
 	}
+	return applied;
+}
+int main(void)
+{
+	Mat img = imread("avrill.jpg");
+	Mat kmDilate = km_dilate(img,9);
+	Mat kmErode = km_erode(img,9);
+	Mat kmsobel = km_sobel(img);
+	namedWindow("km_dilate",0);
+	namedWindow("km_erode",0);
+	namedWindow("kmsobel",0);
+	imshow("km_dilate",kmDilate);
+	imshow("km_erode",kmErode);
+	imshow("kmsobel",kmsobel);
 	waitKey(0);
-	return 0;
+	return(0);
+
 }
